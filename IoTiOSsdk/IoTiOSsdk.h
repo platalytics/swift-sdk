8365f5936237d2776b5a6d542e6facd8279e0f00
Fahad <fnoor@platalytics.com>
2016-05-03 15:13:29 +0500

    Initial Commit

---
+++ IoTiOSsdk/IoTiOSsdk.h
@@ -0,0 +1,19 @@
+//
+//  IoTiOSsdk.h
+//  IoTiOSsdk
+//
+//  Created by Fahad on 03/05/2016.
+//  Copyright © 2016 Platalytics. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IoTiOSsdk.
+FOUNDATION_EXPORT double IoTiOSsdkVersionNumber;
+
+//! Project version string for IoTiOSsdk.
+FOUNDATION_EXPORT const unsigned char IoTiOSsdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IoTiOSsdk/PublicHeader.h>
+
+
